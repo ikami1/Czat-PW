684f605572d32b402d1b032fd92c144c7a18adbf
ikami1 <chicken3579@o2.pl>
2017-01-15 16:00:25 +0100

    Update funkcje.c

--- funkcje.c
+++ funkcje.c
@@ -1,5 +1,9 @@
 #include "specyfikacja.h"
 
+void Reset(){
+  printf("%c[%dm ",0x1B,0); //przywrocenie naturalnych kolorow konsoli
+}
+
 void znajdz_polecenie(char linia[], char polecenie[]){
     int i=0, j=0, n=0;
 
@@ -19,6 +23,16 @@ void znajdz_polecenie(char linia[], char polecenie[]){
         n+=1;
         i+=1;
     }
+}
+
+int znajdzIndeksPokoju(char *tablica[MAX_USERS+1][MAX_GROUPS], char nazwa[]){
+    int i=0,n;
+    for(i=0; i<MAX_GROUPS; i++)
+        if(!strcmp(tablica[0][i],nazwa)){
+            n = i;
+            break;
+        }
+    return n;
 }
 
 int znajdzIndeks(char *tablica[2][MAX_USERS], char nickname[]){
@@ -49,5 +63,14 @@ int czyZajety(char *tablica[2][MAX_USERS], char nickname[]){
         }
     if(!strcmp(nickname,"serwer"))              //pod ta nazwa jest serwer
         return 1;
+    return 0;
+}
+
+int istniejePokoj(char *tablica[MAX_USERS][MAX_GROUPS], char nazwa[]){
+    int i;
+    for(i=0; i<MAX_GROUPS; i++)
+        if(!strcmp(tablica[0][i], nazwa))
+            return 1;
+
     return 0;
 }
